df7ff0b6b7e456b583ee9a23ba2663e08a69aa59
slopjong <git@slopjong.de>
2013-06-01 09:51:01 +0200

    Added the menu and a readme file

---
+++ menu.php
@@ -0,0 +1,10 @@
+<?php
+
+// the menu entries are in the order as listed here
+$menu = array(
+    "intro" => "Home",
+    "directory" => "Directory",
+    "validator" => "Validator",
+    "documentation" => "Documentation",
+    "app" => "Apps"
+);
